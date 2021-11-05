ef85a63f7e3ce819925d0960d9a6436bbe239ebb
Requiem98 <amedeo.ranaldi@gmail.com>
2021-11-05 18:14:19 +0100

    aggiornato

---
+++ aggiornaMain.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Questo script aggiorna il main aggiungendo le modifiche fatte al branch
+
+#Si sposta nella directory di git
+cd ~/Documenti/GitHub_projects/ADM-HW2
+
+
+#seleziona il branch da modificare
+git checkout main
+
+#esegue il merge dei due branch
+git merge ame
+
+#carica le modifiche fatte nello stage		
+git add * 
+
+#Controlla che non siano stati eliminati dei file dalla directory		 
+log=`git commit -m "aggiornato" | awk '/eliminato:/{print $2}'`
+
+#se li trova li elimina da git		
+for i in $log
+	do
+		git rm $i; git commit -m "aggiornato"
+	
+done
+
+#aggiorna repository remota
+git push origin main
---
+++ updateBranch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Questo script aggiorna la directory di github (~/Documenti/GitHub_projects/ADM-HW3)
+
+#Si sposta nella directory di git
+cd ~/Documenti/GitHub_projects/ADM-HW3
+
+#seleziona il branch da modificare
+git checkout ame
+
+#Controlla che non ci siano aggiornamenti dal main e poi carica le modifiche fatte nello stage		
+git pull origin main
+git pull origin ame
+git add * 
+
+#Controlla che non siano stati eliminati dei file dalla directory		 
+log=`git commit -m "aggiornato" | awk '/eliminato:/{print $2}'`
+
+#se li trova li elimina da git		
+for i in $log
+	do
+		git rm $i; git commit -m "aggiornato"
+	
+done
+
+#aggiorna il branch 		
+
+git push origin ame
