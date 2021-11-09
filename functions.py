c443b7930450f5f8b8c8c51bb95393e9bef9b2e1
Requiem98 <amedeo.ranaldi@gmail.com>
2021-11-09 12:39:30 +0100

    aggiornato

--- functions.py
+++ functions.py
@@ -74,3 +74,228 @@ def getAnime(pages, start=0):
     pages_from_start_to_end = pages[start:]
     for i in range(0, len(pages_from_start_to_end)) : 
         save_html_AnimePage_In_ListAnimePage(pages_from_start_to_end[i], start+i+1)
+
+
+"""
+============================================================================================================
+
+    parsing functions
+
+============================================================================================================
+
+"""
+
+def findUsers(string):
+    string = string.split()[3].split(",")
+    
+    if(len(string) < 2):
+        string[0] = string[0][:len(string[0])//2]
+        
+    if(len(string) == 2):
+        temp = string[0].replace(string[1], "")
+        temp = temp[:len(temp)//2]
+        string.insert(0,temp)
+        string.pop(1)
+       
+    
+    if(len(string) == 3):
+        temp = string[0].replace(string[1]+string[2], "")
+        temp = temp[:len(temp)//2]
+        string.insert(0,temp)
+        string.pop(1)
+        
+    users_string = "".join(string)
+    users_integer = int(users_string)
+        
+    return users_integer
+
+
+def tdForCharacters_Voices(tag):
+    return tag.name == "td" and not tag.has_attr("width") and tag.has_attr("valign")
+
+
+def getDataFromPage(pagePath):
+    with open(pagePath, "r") as file:
+        soup = BeautifulSoup(file, "html.parser")
+
+    temp = soup.find_all("div", {"class": "spaceit_pad"}) 
+
+    out=[]
+    tempDict = dict()
+    finalDict = dict()
+
+    for i in temp:
+        out.append(i.text)
+
+    for i in range(0, len(out)):
+        out[i] = out[i].strip()
+        out[i] = out[i].strip("\n")
+        out[i] = out[i].replace("\n", " ")
+
+
+
+    for i in out:
+        index = i.find(":")
+        tempDict[i[:index]] = i[index+1:].strip()
+
+
+
+    if(tempDict["Type"] != "N/A"):
+        finalDict["Type"] = tempDict["Type"]#Anime Type
+    else:
+        finalDict["Type"] = ""
+
+    if(tempDict["Episodes"] != "N/A"):
+        finalDict["Episodes"] = int(tempDict["Episodes"]) #number of episodes
+    else:
+        finalDict["Episodes"] = None
+
+    if(tempDict["Aired"] != "N/A"):
+        finalDict["Aired"]  = tempDict["Aired"]#releasedDate and endDate
+    else:
+        finalDict["Aired"] = None
+
+    if(tempDict["Members"].replace(",", "") != "N/A"):
+        finalDict["Members"] = int(tempDict["Members"].replace(",", "")) #members
+
+    else:
+        finalDict["Members"] = None
+
+    if(tempDict["Score"].split()[3] != "-"):
+        finalDict["Users"] = findUsers(tempDict["Score"])# Users
+    else:
+        finalDict["Users"] = None
+
+    if(tempDict["Score"].split()[0][:-1] != "N/A"):
+        finalDict["Score"] =  float(tempDict["Score"].split()[0])#Score
+    else:
+        finalDict["Score"] = None
+
+    if(tempDict["Ranked"].split()[0].strip("#")[:-1] != "N/A"):
+        finalDict["Ranked"] = int(tempDict["Ranked"].split()[0].strip("#")[:-1])  #Rank
+    else:
+        finalDict["Ranked"] = None
+
+    if(tempDict["Popularity"] != "N/A"):
+        finalDict["Popularity"] = int(tempDict["Popularity"].strip("#")) #Popularity
+    else:
+        finalDict["Popularity"] = None
+
+
+    #Characters
+    #Voices
+    #Staff
+
+    #ADDING THE NAME
+    temp = soup.find("strong") 
+
+    finalDict["Name"] = temp.text
+
+    #ADDING THE SYNOPSIS
+    temp = soup.find("p", itemprop="description")
+
+    finalDict["Synopsis"] = temp.text.replace("\n", " ")
+
+    #ADDING THE RELATED ANIME
+    try:
+        temp = soup.find("table", class_="anime_detail_related_anime", style="border-spacing:0px;")
+        temp = temp.find_all("a")
+
+        yt = set()
+
+        for t in temp:
+            yt.add(t.text)
+
+        finalDict["Related_Anime"] = list(yt)
+
+    except:
+        finalDict["Related_Anime"] = None
+
+    #FIND CHARACTERS, VOICES, STAFF AND ROLE
+    characters = []
+    voices = []
+    staff = []
+    role = []
+
+    try:
+        temp = soup.find_all("div", class_="left-column fl-l divider")
+
+        temp0 = temp[0].find_all("table", width="100%")
+
+        for t in temp0:
+            t = t.find_all(tdForCharacters_Voices)
+
+            try:
+                characters.append(t[0].find("a").string)
+            except:
+                characters.append("")
+            try:
+                voices.append(t[1].find("a").string)
+            except:
+                voices.append("")
+
+        temp1 = temp[1].find_all("table", width="100%")
+
+
+        for t in temp1:
+            t = t.find(tdForCharacters_Voices)
+
+            try:
+                staff.append(t.find("a").string)
+            except:
+                staff.append("")
+
+            try:
+                role.append(t.find("small").string)
+            except:
+                role.append("")
+
+    except:
+        pass
+
+    try:
+        temp = soup.find_all("div", class_="left-right fl-r")
+
+        temp0 = temp[0].find_all("table", width="100%")
+
+        for t in temp0:
+            t = t.find_all(tdForCharacters_Voices)
+
+            try:
+                characters.append(t[0].find("a").string)
+            except:
+                characters.append("")
+            try:
+                voices.append(t[1].find("a").string)
+            except:
+                voices.append("")
+
+
+        temp1 = temp[1].find_all("table", width="100%")
+
+
+        for t in temp1:
+            t = t.find(tdForCharacters_Voices)
+
+            try:
+                staff.append(t.find("a").string)
+            except:
+                staff.append("")
+
+            try:
+                role.append(t.find("small").string)
+            except:
+                role.append("")
+
+    except:
+        characters = None
+        voices = None
+        staff = None
+        role = None
+
+    finalDict["Characters"] = characters
+    finalDict["Voices"] = voices
+    finalDict["Staffs"] = staff
+    finalDict["Roles"] = role
+
+    return finalDict
